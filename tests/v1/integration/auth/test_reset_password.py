34421ddf43a04802e742c069944f50ff2c9cecce
Val-Priad <valeriipriadchenko@gmail.com>
2026-01-18 22:48:27 +0100

    finished tests for forgot password use case

---
+++ tests/v1/integration/auth/test_reset_password.py
@@ -0,0 +1,68 @@
+from datetime import datetime, timedelta, timezone
+
+import pytest
+from sqlalchemy import select
+
+from domain.password_reset.password_reset_model import PasswordReset
+from domain.user.user_model import User
+
+
+@pytest.fixture
+def any_user(db_session):
+    user = User(email="user@example.com", password_hash=b"hash")
+
+    db_session.add(user)
+    db_session.flush()
+
+    password_reset = PasswordReset(
+        user_id=user.id,
+        token_hash=b"11111",
+        expires_at=datetime.now(timezone.utc) + timedelta(hours=5),
+    )
+    db_session.add(password_reset)
+    db_session.flush()
+    return {"user_id": user.id, "user_email": user.email}
+
+
+def test_reset_password_token_valid(client, any_user, db_session):
+    response = client.post(
+        "/api/v1/auth/reset-password", json={"email": any_user["user_email"]}
+    )
+
+    assert response.status_code == 201
+
+    qty_of_active_tokens = len(
+        db_session.scalars(
+            select(PasswordReset).where(
+                PasswordReset.expires_at >= datetime.now(timezone.utc),
+                PasswordReset.user_id == any_user["user_id"],
+            )
+        ).all()
+    )
+    assert qty_of_active_tokens == 1
+
+    qty_of_tokens = len(
+        db_session.scalars(
+            select(PasswordReset).where(
+                PasswordReset.user_id == any_user["user_id"],
+            )
+        ).all()
+    )
+    assert qty_of_tokens == 2
+
+
+def test_reset_password_token_no_user(client):
+    response = client.post(
+        "/api/v1/auth/reset-password", json={"email": "user@example.com"}
+    )
+
+    assert response.status_code == 400
+
+
+def test_reset_password_token_invalid_email(client):
+    response = client.post(
+        "/api/v1/auth/resend-verification",
+        json={"email": "invalid_email"},
+    )
+
+    assert response.status_code == 400
---
+++ tests/v1/integration/auth/test_verify_new_password.py
@@ -0,0 +1,103 @@
+import secrets
+from datetime import datetime, timedelta, timezone
+
+import pytest
+from sqlalchemy import select
+
+from domain.password_reset.password_reset_model import PasswordReset
+from domain.password_reset.password_reset_service import PasswordResetService
+from domain.user.user_model import User
+
+
+@pytest.fixture()
+def set_token(db_session):
+    user_old_hash = b"hash"
+    user = User(email="user@example.com", password_hash=user_old_hash)
+
+    db_session.add(user)
+    db_session.flush()
+
+    raw_token = secrets.token_urlsafe(32)
+
+    password_reset = PasswordReset(
+        user_id=user.id,
+        token_hash=PasswordResetService._get_hashed_token(raw_token),
+        expires_at=datetime.now(timezone.utc) + timedelta(hours=5),
+    )
+    db_session.add(password_reset)
+    db_session.flush()
+    return {
+        "user_id": user.id,
+        "user_old_hash": user_old_hash,
+        "raw_token": raw_token,
+    }
+
+
+def test_verify_new_password_valid(client, set_token, db_session):
+    response = client.post(
+        "/api/v1/auth/verify-new-password",
+        json={
+            "token": set_token["raw_token"],
+            "password": "new_password",
+        },
+    )
+
+    assert response.status_code == 200
+
+    user = db_session.scalar(
+        select(User).where(User.id == set_token["user_id"])
+    )
+
+    user_new_password_hash = user.password_hash
+
+    assert set_token["user_old_hash"] != user_new_password_hash
+
+    password_reset = db_session.scalar(
+        select(PasswordReset).where(
+            PasswordReset.user_id == set_token["user_id"]
+        )
+    )
+
+    assert datetime.now(timezone.utc) > password_reset.used_at
+
+    response = client.post(
+        "/api/v1/auth/verify-new-password",
+        json={
+            "token": set_token["raw_token"],
+            "password": "111111111111111",
+        },
+    )
+
+    assert response.status_code == 400
+
+
+def test_verify_new_password_validation(client, set_token, db_session):
+    response = client.post(
+        "/api/v1/auth/verify-new-password",
+        json={
+            "token": set_token["raw_token"],
+        },
+    )
+
+    assert response.status_code == 400
+
+    response = client.post(
+        "/api/v1/auth/verify-new-password",
+        json={"token": set_token["raw_token"], "password": ""},
+    )
+
+    assert response.status_code == 400
+
+    response = client.post(
+        "/api/v1/auth/verify-new-password",
+        json={"password": "valid_password"},
+    )
+
+    assert response.status_code == 400
+
+    response = client.post(
+        "/api/v1/auth/verify-new-password",
+        json={"password": "valid_password", "token": "invalid_token"},
+    )
+
+    assert response.status_code == 400
