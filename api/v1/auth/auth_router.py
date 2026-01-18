d88e21fffae57893798b1d0606255bced9d94035
Val-Priad <valeriipriadchenko@gmail.com>
2026-01-18 17:18:03 +0100

    refactoring

--- api/v1/auth/auth_router.py
+++ api/v1/auth/auth_router.py
@@ -39,87 +39,6 @@ def construct_error(code: str) -> Response:
     )
 
 
-@bp.post("/login")
-def login():
-    db = session()
-    try:
-        data = EmailPasswordRequest.model_validate(request.json)
-
-        user = auth_service.verify_password(db, data.email, data.password)
-
-        access_token = create_access_token(identity=user.id, fresh=True)
-        response = construct_response(
-            message="Logged in successfully",
-            status=200,
-        )
-        set_access_cookies(response, access_token)
-        return response
-    except ValidationError:
-        return construct_error("validation_error")
-    except Exception as e:
-        db.rollback()
-        return construct_error(get_code_for_exception(e))
-    finally:
-        db.close()
-
-
-@bp.post("/logout")
-@jwt_required()
-def logout():
-    response = jsonify({"message": "Logout successful"})
-    unset_jwt_cookies(response)
-    return response
-
-
-@bp.post("/resend-verification")
-def resend_verification():
-    db = session()
-    try:
-        data = SendNewValidationTokenRequest.model_validate(request.json)
-
-        user = email_verification_service.get_user_by_email(db, data.email)
-
-        email_verification_service.check_user_is_not_verified(user)
-
-        raw_token = email_verification_service.get_resend_token(db, user.id)
-
-        db.commit()
-
-        email_verification_service.send_verification_email(
-            user.email, raw_token
-        )
-
-        return construct_response(
-            message="Check your email for confirmation link", status=201
-        )
-    except ValidationError:
-        return construct_error("validation_error")
-    except Exception as e:
-        db.rollback()
-        return construct_error(get_code_for_exception(e))
-    finally:
-        db.close()
-
-
-@bp.post("/verify-email")
-def verify_token():
-    db = session()
-    try:
-        data = VerifyTokenRequest.model_validate((request.json))
-        email_verification_service.verify_token(db, data.token)
-        db.commit()
-        return construct_response(
-            message="Verification is successful", status=200
-        )
-    except ValidationError:
-        return construct_error("validation_error")
-    except Exception as e:
-        db.rollback()
-        return construct_error(get_code_for_exception(e))
-    finally:
-        db.close()
-
-
 @bp.post("/register")
 def register():
     db = session()
@@ -152,3 +71,84 @@ def register():
         return construct_error(get_code_for_exception(e))
     finally:
         db.close()
+
+
+@bp.post("/verify-email")
+def verify_token():
+    db = session()
+    try:
+        data = VerifyTokenRequest.model_validate((request.json))
+        email_verification_service.verify_token(db, data.token)
+        db.commit()
+        return construct_response(
+            message="Verification is successful", status=200
+        )
+    except ValidationError:
+        return construct_error("validation_error")
+    except Exception as e:
+        db.rollback()
+        return construct_error(get_code_for_exception(e))
+    finally:
+        db.close()
+
+
+@bp.post("/resend-verification")
+def resend_verification():
+    db = session()
+    try:
+        data = SendNewValidationTokenRequest.model_validate(request.json)
+
+        user = email_verification_service.get_user_by_email(db, data.email)
+
+        email_verification_service.check_user_is_not_verified(user)
+
+        raw_token = email_verification_service.get_resend_token(db, user.id)
+
+        db.commit()
+
+        email_verification_service.send_verification_email(
+            user.email, raw_token
+        )
+
+        return construct_response(
+            message="Check your email for confirmation link", status=201
+        )
+    except ValidationError:
+        return construct_error("validation_error")
+    except Exception as e:
+        db.rollback()
+        return construct_error(get_code_for_exception(e))
+    finally:
+        db.close()
+
+
+@bp.post("/login")
+def login():
+    db = session()
+    try:
+        data = EmailPasswordRequest.model_validate(request.json)
+
+        user = auth_service.verify_password(db, data.email, data.password)
+
+        access_token = create_access_token(identity=user.id, fresh=True)
+        response = construct_response(
+            message="Logged in successfully",
+            status=200,
+        )
+        set_access_cookies(response, access_token)
+        return response
+    except ValidationError:
+        return construct_error("validation_error")
+    except Exception as e:
+        db.rollback()
+        return construct_error(get_code_for_exception(e))
+    finally:
+        db.close()
+
+
+@bp.post("/logout")
+@jwt_required()
+def logout():
+    response = jsonify({"message": "Logout successful"})
+    unset_jwt_cookies(response)
+    return response
