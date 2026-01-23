a58abf022994f733ff1c2cc05b24fb0de0728a43
Val-Priad <valeriipriadchenko@gmail.com>
2026-01-23 17:55:06 +0100

    fixed password validation logic

--- schemas/types.py
+++ schemas/types.py
@@ -2,10 +2,12 @@ from typing import Annotated
 
 from pydantic import BeforeValidator, EmailStr, Field
 
-from .validators.user_validators import strip_password
+from .validators.user_validators import reject_password_with_whitespaces
 
 UserEmail = Annotated[EmailStr, Field(min_length=1, max_length=255)]
 Password = Annotated[
-    str, Field(min_length=8, max_length=255), BeforeValidator(strip_password)
+    str,
+    Field(min_length=8, max_length=255),
+    BeforeValidator(reject_password_with_whitespaces),
 ]
 Token = Annotated[str, Field(min_length=40, max_length=50)]
--- schemas/validators/user_validators.py
+++ schemas/validators/user_validators.py
@@ -1,4 +1,4 @@
-def strip_password(value: str) -> str:
-    if isinstance(value, str):
-        value = value.strip()
+def reject_password_with_whitespaces(value: str) -> str:
+    if isinstance(value, str) and any(c.isspace() for c in value):
+        raise ValueError("There must be no spaces in the password")
     return value
