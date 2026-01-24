acc0b6319b08ef288b2de2417d12a08e3b663cef
Val-Priad <valeriipriadchenko@gmail.com>
2026-01-24 13:14:46 +0100

    changed types related to user in schemas. Because person must be able to update his data with empty strings

--- schemas/types.py
+++ schemas/types.py
@@ -9,12 +9,12 @@ from .validators.user_validators import reject_string_with_whitespaces
 
 UserName = Annotated[
     str,
-    Field(min_length=1, max_length=255),
+    Field(max_length=255),
     BeforeValidator(reject_string_with_whitespaces),
 ]
 PhoneNumberType = Annotated[PydanticPhoneNumber, Field()]
-ImageKey = Annotated[str, Field(min_length=1, max_length=1024)]
-UserDescription = Annotated[str, Field(min_length=1, max_length=510)]
+ImageKey = Annotated[str, Field(max_length=1024)]
+UserDescription = Annotated[str, Field(max_length=510)]
 UserEmail = Annotated[EmailStr, Field(min_length=1, max_length=255)]
 Password = Annotated[
     str,
